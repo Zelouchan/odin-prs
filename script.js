a6b862d0198fcdaf4a001fb16c4a3c9cc28a8a82
Zelouchan <bad1dje@gmail.com>
2025-01-05 19:53:38 +0000

    Light on the horizon

--- script.js
+++ script.js
@@ -19,69 +19,45 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        if (button.id === "paper") {
-            userChoice = button.id; 
-        } else if (button.id === "rock") {
-            userChoice = button.id; 
-        } else if (button.id === "scissors") {
-            userChoice = button.id; 
-        }
+        userChoice = button.id; 
+        singleRound();
         });
     });
 
+    function singleRound() {
+        let computerChoice = getComputerChoice();
+        const result = playRound(userChoice, computerChoice);
+        updateResult(result);
+        updateMessage(result, userChoice, computerChoice);
+        updateChoices(userChoice, computerChoice);
+        };
+        
 
-// loops the game
-// const loops = Math.max(computerScore, humanScore);
-
-  function playGame() {
-    for (let i = 0; i < 5; i++)
-     {
-        // caluclates random number and gives value
-    function getComputerChoice() {
-     let result = Math.floor(Math.random() * 3) + 1;
-
-      if (result === 1) {
-        return "rock";
-        } else if (result === 2) {
-        return "paper";
-       } else {
-        return "scissors";
-        }
-    }
-
-        const computerChoice = getComputerChoice();
-        const humanChoice = userChoice;
-
-
-        // getHumanChoice();
-        getComputerChoice();
-
-
-// plays the actual game
-        function playRound() {
-            if        (humanChoice === computerChoice) {
+        function playRound(userChoice, computerChoice) {
+            if        (userChoice === computerChoice) {
                 return 'draw'}
                 
-            else if (humanChoice == 'paper' && computerChoice == "rock") {
-                return 'win'} 
-            else if (humanChoice == 'rock' && computerChoice == "scissors") {
-                return 'win'} 
-            else if (humanChoice == 'scissors' && computerChoice == "paper") {
-                return 'win';}
+            else if ((userChoice == 'paper' && computerChoice == "rock") ||
+            (userChoice == 'rock' && computerChoice == "scissors") ||
+            (userChoice == 'scissors' && computerChoice == "paper")) {
+                return 'win'}
                 
             else {
                 return 'lose';}
             }
 
-            // prints the choices of player and computer to the page
-            const containers = document.querySelector(".chosen");
-            const contents = document.createElement("div");
-            contents.classList.add(".choices");
-            contents.textContent = ("You picked " + userChoice + " and computer picked "+ computerChoice);
-            containers.appendChild(contents);
-
-        playRound(humanChoice, computerChoice);
-
+    function getComputerChoice() {
+        let result = Math.floor(Math.random() * 3) + 1;
+   
+         if (result === 1) {
+           return "rock";
+           } else if (result === 2) {
+           return "paper";
+          } else {
+           return "scissors";
+           }
+       }  
+       
         // win or lose message
         function message() {
             if    (playRound() == 'draw') { 
@@ -90,29 +66,35 @@ buttons.forEach((button) => {
             else if (playRound() == 'win') {
                    return ("You Win! " + humanChoice + " beats " + computerChoice + "." ); }          
             else { 
-                    return ("You Lost! " + computerChoice + " beats " + computerChoice + ".");
+                    return ("You Lost! " + computerChoice + " beats " + humanChoice + ".");
                             }
              }
 
-        // displays the win or lose message WORKING FINE
+        // displays the win or lose message
         const container1 = document.querySelector(".result");
         const content1 = document.createElement("div");
         content1.classList.add(".result");
         content1.textContent = message();
         container1.appendChild(content1);
                             
-        function score() {
-            let result = playRound();
-            if  (result === 'win') {
-                humanScore++;
-                }
-            else if (result === 'lose') {
-                computerScore++;
-                }               
-            return {computerScore, humanScore}}
-            console.log(score);
+//         function score() {
+//             let result = playRound();
+//             if  (result === 'win') {
+//                 humanScore++;
+//                 }
+//             else if (result === 'lose') {
+//                 computerScore++;
+//                 }               
+//             return {computerScore, humanScore}}
+//             console.log(score);
             
-        }
-  }  
+//         }
+//   }  
+// prints the choices of player and computer to the page
+const containers = document.querySelector(".chosen");
+const contents = document.createElement("div");
+contents.classList.add(".choices");
+contents.textContent = ("You picked " + userChoice + " and computer picked "+ computerChoice);
+containers.appendChild(contents);
     
-playGame(); 
+// playGame(); 
